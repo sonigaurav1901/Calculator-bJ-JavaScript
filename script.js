e8b30d3b1a9711f11b2980bbfe132b246432beec
Divyam Saraswat <53369231+divyam-saraswat@users.noreply.github.com>
2020-09-30 22:36:20 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+class Calculator{
+    constructor();
+}
+//undone js work
+
+
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButtons = document.querySelector('[data-equals]');
+const deleteButtons = document.querySelector('[data-delete]');
+const allClearButtons = document.querySelector('[data-equals]');
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+const currentOperandTextElement = document.querySelector('[data-current-operand]');
+
